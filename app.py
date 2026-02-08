de1cc8190d42e974a34c09c79cf81435bb64abe6
nineocto2046 <nine.octo.2046@gmail.com>
2026-02-09 01:02:15 +0800

    Delete app.py - no longer needed for static HTML deployment

--- app.py
+++
@@ -1,3 +0,0 @@
-# This file is no longer needed - using static HTML deployment
-# Deleted as part of migration to Netlify static hosting
-# To remove this file completely, delete it from the repository
