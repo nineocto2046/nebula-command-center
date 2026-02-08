51797df169e25aa019a7ec4f1c2d76b444470a51
nineocto2046 <nine.octo.2046@gmail.com>
2026-02-09 01:01:10 +0800

    Mark app.py as deprecated (to be deleted)

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 # This file is no longer needed - using static HTML deployment
 # Deleted as part of migration to Netlify static hosting
+# To remove this file completely, delete it from the repository
